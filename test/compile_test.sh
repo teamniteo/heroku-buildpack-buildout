a31b7d367debc466f2fe1eddc019448968ec2bf6
Viet Dinh <vietdt@viu.(none)>
2016-05-26 06:36:46 +0700

    setup correct environment before compiling

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -4,7 +4,26 @@
 
 testCompile()
 {
-  capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+  if [ ! -d $BUILD_DIR/.heroku ]; then
+      # this test is not running on heroku
+      # use virtualenv to create python inside BUILD_DIR/.heroku
+      mkdir -p ${BUILD_DIR}/.heroku/python
+      virtualenv ${BUILD_DIR}/.heroku/python
+  fi
+
+  # copy default bootstrap.py & buildout.cfg to BUILD_DIR
+  cp ${BUILDPACK_HOME}/bootstrap.py ${BUILD_DIR}/bootstrap.py
+  cp ${BUILDPACK_HOME}/buildout.cfg ${BUILD_DIR}/buildout.cfg
+
+  # XXX: copy cached eggs
+  cp -r ${BUILDPACK_HOME}/cache/eggs ${BUILD_DIR}/
+
+  # remove python symlink in /app/.heroku/ if existed before compiling
+  if [ -L "/app/.heroku/python" ]; then
+      rm -f /app/.heroku/python
+  fi
+
+  compile
   assertEquals 0 ${rtrn}
-  assertEquals "" "`cat ${STD_ERR}`"
+  assertCaptured "Done"
 }
