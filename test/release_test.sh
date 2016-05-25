e5823ff9576bb4f426b8c7bf6f6b32bbe41c1532
Viet Dinh <vietdt@viu.(none)>
2016-05-26 06:34:46 +0700

    clean up

--- test/release_test.sh
+++ test/release_test.sh
@@ -4,12 +4,7 @@
 
 testRelease()
 {
-  expected_release_output=`cat <<EOF
----
-EOF`
-
-  capture ${BUILDPACK_HOME}/bin/release ${BUILD_DIR}
-  assertEquals 0 ${rtrn}
-  assertEquals "${expected_release_output}" "$(cat ${STD_OUT})"
-  assertEquals "" "$(cat ${STD_ERR})"
+  release
+  assertCapturedSuccess
+  assertCapturedEquals "---"
 }
